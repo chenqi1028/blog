dffb2356c64defe73a8d118ae28b994582d429d2
chenqi <chenqi@custouch.com>
2020-09-28 18:37:55 +0800

    提交

--- src/views/Note.vue
+++ src/views/Note.vue
@@ -24,7 +24,7 @@ export default {
     },
     setup(){
         let list = ref(data)
-        return {list}
+        return { list }
     },
     mounted(){
         console.log(data);
---
+++ vite.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    cssPreprocessOptions: {
+    //   less: {
+    //     modifyVars: {
+    //       'preprocess-custom-color': 'green'
+    //     }
+    //   }
+    }
+  }
